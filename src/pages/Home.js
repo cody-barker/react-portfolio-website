77dc93311dff558179efb456aeeaa428b522dd26
Cody <codybarker.or@gmail.com>
2023-12-12 10:32:49 -0800

    make page responsive for smaller screen sizes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,27 +6,7 @@ import About from '../components/About.js'
 
 function Home() {
 
-  function onClick(url) {
-    window.open(url, '_blank')
-  }
-
   return(
-    // <div className="container">
-    //   <div className="home-left">
-    //     <img src={headshot} alt="headshot"/>
-    //   </div>
-    //   <div className="home-right">
-    //     <h1>Cody Barker</h1>
-    //     <p>Full-Stack Software Engineer</p>
-    //     <p>React | JavaScript | Ruby on Rails</p>
-    //     <div className="icons-container">
-    //       <button onClick={() => onClick('https://drive.google.com/file/d/1E1DjQEfq_jVlV4agaku-KVr_8c2LDmM3/view?usp=drive_link')}>Resume</button>
-    //       <img id="no-shadow" src={linkedInIcon} alt="LinkedIn icon" onClick={() => onClick('https://www.linkedin.com/in/barkercody/')}/>
-    //       <img id="no-shadow" src={githubIcon} alt="GitHub icon" onClick={() => onClick('https://github.com/cody-barker')}/>
-    //       <img id="no-shadow" src={devToIcon} alt="Dev.to icon" onClick={() => onClick('https://dev.to/codybarker')}/>
-    //     </div>
-    //   </div>
-    // </div>
     <About id="about"/>
   )
 }

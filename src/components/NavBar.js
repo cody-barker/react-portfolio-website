249f167eedfe09ce27e3b0e0e2fd1bb167d9d20a
Cody <codybarker.or@gmail.com>
2023-12-12 13:42:46 -0800

    style header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,9 @@ function NavBar() {
         <nav class="nav">
             <h1>Cody Barker</h1>
             <ul>
-                {/* <li><h1>Cody Barker</h1></li> */}
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><a to="#about">About</a></li>
                 <li><a to="#projects">Projects</a></li>
-
             </ul>
         </nav>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,18 @@ import devToIcon from '../assets/brand-dev.to.svg'
 import NavBar from '../components/NavBar.js'
 import About from '../components/About.js'
 import Projects from '../components/Projects.js'
+import jungle from '../assets/jungle.jpg'
 
 function Home() {
 
   return(
     <>
       <NavBar />
-      <header className="main-header"></header>
+      <header className="header-wrap">
+        <div className="header-content">
+          <h2>Full-Stack Developer</h2>
+        </div>
+      </header>
       <About id="about"/>
       <Projects id="projects"/>
     </>

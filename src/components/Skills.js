773e3fd5cfb1b9efe21e7533e835c007cf2409ce
Cody <codybarker.or@gmail.com>
2023-12-13 13:25:32 -0800

    update skills cards

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,9 +5,42 @@ function Skills() {
     <section className="skills-container">
       <div className="skills-card">
         <h2>Frontend</h2>
+        <ul className="skills-list">
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>React Context API</li>
+          <li>Single Page Applications</li>
+        </ul>
       </div>
       <div className="skills-card">
         <h2>Backend</h2>
+        <ul className="skills-list">
+          <li>Ruby on Rails</li>
+          <li>RESTful APIs</li>
+          <li>Active Record</li>
+          <li>Validations and Serialization</li>
+          <li>Authentication & Authorizations</li>
+          <li>Relational Databases</li>
+          <li>PostgreSQL</li>
+          <li>Sessions</li>
+          <li>MVC</li>
+        </ul>
+      </div>
+      <div className="skills-card">
+        <h2>Extended Skills</h2>
+        <ul className="skills-list">
+          <li>Git</li>
+          <li>Github</li>
+          <li>Deployment</li>
+          <li>Arduino IDE</li>
+          <li>Asana</li>
+          <li>Inkscape</li>
+          <li>Project Management</li>
+          <li>Research</li>
+        </ul>
       </div>
     </section>
   )

a62fdf831b6a05a1044549fb383665165c9a65bb
Cody <codybarker.or@gmail.com>
2023-12-14 14:53:09 -0800

    add basic style to 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,13 @@
 
+import {NavLink} from 'react-router-dom'
+
 function NotFound() {
   return(
     <div className="container">
       <p className="large-text">404 Not Found</p>
       <p>That URL is incorrect.</p>
       <p>Please navigate back to the home page.</p>
+      <NavLink to="/">Home</NavLink>
     </div>
   )
 }
